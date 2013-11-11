611736967f2dfd9e11766ea3c3ca900dd73013d9
Christopher Harris <ccharri@umich.edu>
2013-11-10 22:08:19 -0500

    Tower Placement Mechanic Basics in.

---
+++ EECS494-Assignment4/Assets/PlacementManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlacementManager : MonoBehaviour {
+	public GameObject placePrefab;
+	public bool placing = false;
+	private GameObject placeObject;
+	
+	public float gridSize = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(placing)
+		{
+			RaycastHit rhit;
+			if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rhit))
+			{
+				Vector3 point = alignToGrid (rhit.point);
+				placeObject.transform.position = point;
+				
+				if(Input.GetMouseButtonDown(0))
+				{
+					place(rhit.point);
+				}
+			}
+		}
+	}
+
+	void OnGUI() {
+		if(!placing)
+		{
+			if(GUI.Button(new Rect(20, Screen.height-70, 100, 50), "Place a box"))
+			{
+				placing = true;
+				placeObject = (GameObject)Instantiate(placePrefab);
+				placeObject.layer = 2;
+			}
+		}
+	}
+
+	void place(Vector3 point)
+	{
+		Destroy(placeObject);
+		placeObject = null;
+		Instantiate(placePrefab, alignToGrid(point), Quaternion.identity);
+		placing = false;
+	}
+	
+	Vector3 alignToGrid(Vector3 point)
+	{
+		Vector3 rval;
+		rval.y = 0;
+		rval.x = ((int)((Mathf.Abs(point.x) / gridSize) + .5)) * gridSize * (Mathf.Abs (point.x)/point.x) ;
+		rval.z = ((int)((Mathf.Abs(point.z) / gridSize) + .5)) * gridSize * (Mathf.Abs(point.z)/point.z) ;
+		return rval;
+	}
+}
