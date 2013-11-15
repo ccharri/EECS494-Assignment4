b7ad18dee8291d287be2e0a78a253e03a6cdd848
Christopher Harris <ccharri@umich.edu>
2013-11-15 00:25:00 -0500

    Continue Merge

--- EECS494-Assignment4/Assets/Projectile.cs
+++ EECS494-Assignment4/Assets/Projectile.cs
@@ -7,22 +7,22 @@ public abstract class Projectile : Unit
     Creep target;
     Vector3 targetPos;
 
-<<<<<<< HEAD
-	public float getSpeed()
-	{
-		return (float)speed.get();
-	}
-=======
     public Projectile()
     {
         tag = "Projectile";
     }
->>>>>>> 4b82ab7fcb6c7f937b14026cdeca08b4f25d09e1
+
+
+	public float getSpeed()
+	{
+		return (float)speed.get();
+	}
 
 	public override void Update() 
     {
         base.Update();
 	}
+
     public override void FixedUpdate()
     {
         base.FixedUpdate();
