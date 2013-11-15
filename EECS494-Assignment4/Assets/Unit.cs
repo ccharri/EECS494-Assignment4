e198e6cefb65ed6867b0a4ca61176f7cd54b1967
Christopher Harris <ccharri@umich.edu>
2013-11-15 16:12:58 -0500

    Fixed Unit

--- EECS494-Assignment4/Assets/Unit.cs
+++ EECS494-Assignment4/Assets/Unit.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 public abstract class Unit : MonoBehaviour 
 {
 	public string name; //This is an internal name used for mapping and RPC calls
-	protected int ownerID;
+	protected string ownerID;
     protected List<Buff<Unit>> buffs;
     //TODO: Consider changing this to a map from BuffTag (string) to Buff for faster lookup
 
-	public int getOwnerID() {return ownerID;}
+	public string getOwnerID() {return ownerID;}
 
     public void Start()
     {
         buffs = new List<Buff<Unit>>();
     }
 
-    public void Init(string name_, int ownerID_)
+    public void Init(string name_, string ownerID_)
     //NOTE: Init is only used when parameters need to be passed in.
     {
         name = name_;
