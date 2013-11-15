e805169d32919d802c83146c69947941efaa2b43
Thomas Dorsey <twdorsey@umich.edu>
2013-11-15 16:17:08 -0500

    Unbroke SampleTest

--- EECS494-Assignment4/Assets/Tests/SampleTest.cs
+++ EECS494-Assignment4/Assets/Tests/SampleTest.cs
@@ -2,19 +2,23 @@
 using System.Collections;
 
 public class SampleTest : MonoBehaviour {
+    public GameObject sampleTower;
 
 	// Use this for initialization
 	void Start () {
         GameState g = GameState.getGameState();
-        g.addPlayer(1);
-        g.addPlayer(2);
+        g.addPlayer("1");
+        g.addPlayer("2");
 
-        GameObject tower = Instantiate(Resources.Load("Towers/ArcaneTower"), new Vector3(0, 0, 0), new Quaternion()) as GameObject;
-        GameObject creep = Instantiate(Resources.Load("Creeps/InfamousCrate"), new Vector3(0, 0, 0), new Quaternion()) as GameObject;
+        GameObject tower = Instantiate(sampleTower, new Vector3(0, 0, 0), new Quaternion()) as GameObject;
+        Tower t = tower.GetComponent<Tower>();
+        g.spawnTowerForPlayer("1", t);
+
+        GameObject creep = Instantiate(sampleTower, new Vector3(0, 0, 0), new Quaternion()) as GameObject;
         Creep c = creep.GetComponent<Creep>();
         c.Init(100, 10, 10, 1);
-        c.Init("Infamous Crate", 2);
-
+        c.Init("Infamous Crate", "2");
+        g.spawnCreepForPlayer("1", c);
         
 	}
 	
