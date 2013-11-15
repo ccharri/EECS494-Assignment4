f0b0227cb842ed35d3ab848e5372a4bdc8064055
David Sabourin <davidjns@umich.edu>
2013-11-15 13:03:40 -0500

    Tooltip Code (in dummy classes)
    Created a dummy Creep and Tower class that derive from the abstract
    classes so that I can attach the scripts to our "Placeable" object in
    Unity.

---
+++ EECS494-Assignment4/Assets/DummyTower.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyTower : Tower {
+
+    private string toolTipText = "";
+    private GUIStyle guiStyleFore;
+    private GUIStyle guiStyleBack;
+
+	// Use this for initialization
+	void Start () {
+      guiStyleFore = new GUIStyle();
+      guiStyleFore.normal.textColor = Color.white;
+      guiStyleFore.alignment = TextAnchor.UpperLeft;
+      guiStyleFore.wordWrap = true;
+
+      guiStyleBack = new GUIStyle();
+      guiStyleBack.normal.textColor = Color.black;
+      guiStyleBack.alignment = TextAnchor.UpperLeft;
+      guiStyleBack.wordWrap = true;	
+	}
+
+  public override string getDescription()
+  {
+      string description = "Object: " + name + "\nTargeting: ";
+
+      if (target)
+          description += target.name;
+      else
+          description += "Nothing";
+      return description;
+  }
+
+  public void OnMouseEnter()
+  {
+      toolTipText = getDescription();
+      System.Console.WriteLine("on mouse enter!!!");
+  }
+
+  public void OnMouseExit()
+  {
+      toolTipText = "";
+  }
+
+  public void OnGUI()
+  {
+      if (toolTipText != "")
+      {
+          var x = Event.current.mousePosition.x;
+          var y = Event.current.mousePosition.y;
+
+          var rectBack = new Rect(x - 99, y + 21, 300, 60);
+          var rectFore = new Rect(x - 100, y + 20, 300, 60);
+
+          GUI.Box(rectBack, new GUIContent());
+
+          GUI.Label(rectBack, toolTipText, guiStyleBack);
+          GUI.Label(rectFore, toolTipText, guiStyleFore);
+      }
+  }
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
