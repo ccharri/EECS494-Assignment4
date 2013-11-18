1226630cf8dac388f6982ff55dfe983b87cbb978
Steven Lebioda <stevenlebioda@gmail.com>
2013-11-18 16:09:29 -0500

    Base camera movement - right click + drag

---
+++ EECS494-Assignment4/Assets/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	bool rightDown;
+	GameObject mainCamera;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rightDown = false;
+		mainCamera = GameObject.FindWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		rightDown = Input.GetMouseButton(1);
+	}
+
+	void LateUpdate()
+	{
+		if(rightDown) //1 = right mouse button
+		{
+			float MouseX = Input.GetAxis("Mouse X");
+			float MouseY = Input.GetAxis("Mouse Y");
+			Vector3 CameraPos = new Vector3(- MouseX, 0, - MouseY);
+			 
+			mainCamera.transform.position += CameraPos;
+		}
+	}
+}
