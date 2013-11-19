fc5bf2bcf11ec8ea9493e896914e6411eb289f1d
Christopher Harris <ccharri@umich.edu>
2013-11-19 18:09:39 -0500

    Create Prefab Link

--- EECS494-Assignment4/Assets/Creeps/InfamousCrate.cs
+++ EECS494-Assignment4/Assets/Creeps/InfamousCrate.cs
@@ -5,6 +5,7 @@ public class InfamousCrate : Creep
 {
     public void Init()
     {
+		prefab = Resources.Load ("Creeps/InfamousCreate") as GameObject;
         Init("Infamous Crate", Network.player.guid, 100, 10, 10, 1);
     }
 }
