a8193d9f9a57d127ed5007a149e0c50e66483c25
Christopher Harris <ccharri@umich.edu>
2013-11-21 01:37:46 -0500

    Made a VERY Basic Arcane Tower

---
+++ EECS494-Assignment4/Assets/TestAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAnimation : MonoBehaviour {
+
+	public AnimationClip idle;
+
+	// Use this for initialization
+	void Start () {
+		animation.AddClip(idle, "Idle");
+		animation.wrapMode = WrapMode.Loop;
+		animation.CrossFade("Idle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
