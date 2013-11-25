36fd873e8ca2a05e0deeb5fb0cb8186de51c43c3
jobob2992 <joesteve@umich.edu>
2013-11-25 13:48:42 -0500

    compile fix, safe

--- EECS494-Assignment4/Assets/ZGridMaker.cs
+++ EECS494-Assignment4/Assets/ZGridMaker.cs
@@ -5,16 +5,16 @@ public class ZGridMaker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		new List<GameObject> ZGrid;
+		/*new List<GameObject> ZGrid;
 		for (int i = -138; i < 140; i += 2)
 		{
 			LineRenderer renderer;
 			new GameObject line;
 			renderer = gameObject.AddComponent<LineRenderer>();
 			renderer.useWorldSpace = true;
-			renderer.material = 
-			ZGrid.Add();
-		}
+			//renderer.material = 
+			//ZGrid.Add();
+		}*/
 	}
 	
 	// Update is called once per frame
