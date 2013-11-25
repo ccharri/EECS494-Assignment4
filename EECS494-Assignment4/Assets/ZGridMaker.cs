7a2efb91764edf67785ecf80af9713fa02eec1b0
jobob2992 <joesteve@umich.edu>
2013-11-25 13:45:17 -0500

    untracked files

---
+++ EECS494-Assignment4/Assets/ZGridMaker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZGridMaker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		new List<GameObject> ZGrid;
+		for (int i = -138; i < 140; i += 2)
+		{
+			LineRenderer renderer;
+			new GameObject line;
+			renderer = gameObject.AddComponent<LineRenderer>();
+			renderer.useWorldSpace = true;
+			renderer.material = 
+			ZGrid.Add();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
