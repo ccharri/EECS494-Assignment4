97d537c124e5c1d048608cad0f5809e9dc75c931
David Sabourin <davidjns@umich.edu>
2013-12-03 20:34:28 -0500

    Pessimistic Commit
    woot

--- EECS494-Assignment4/Assets/GameState.cs
+++ EECS494-Assignment4/Assets/GameState.cs
@@ -945,10 +945,9 @@ public class GameState : MonoBehaviour
 
 	void setStock(int stock_, NetworkPlayer player_, string creepName_)
 	{
-		spawns[player_.guid].getSpawn(creepName_).currentStock = stock_;
+		//spawns[player_.guid].getSpawn(creepName_).currentStock = stock_;
 
-    //var us = Race.getUnitSpawnMap(player_.guid)[creepName_];
-    //us.currentStock = stock_;
+    var us = Race.getUnitSpawnMap(player_.guid)[creepName_].currentStock = stock_;
 	}
 
 	//setStockTimer
@@ -967,10 +966,9 @@ public class GameState : MonoBehaviour
 
 	void setStockTimer(float lastTime_, NetworkPlayer player_, string creepName_)
 	{
-		spawns[player_.guid].getSpawn(creepName_).lastRestock = lastTime_;
+		//spawns[player_.guid].getSpawn(creepName_).lastRestock = lastTime_;
 
-    //var us = Race.getUnitSpawnMap(player_.guid)[creepName_];
-    //us.lastRestock = lastTime_;
+      us = Race.getUnitSpawnMap(player_.guid)[creepName_].lastRestock = lastTime_;
 	}
 
 }
