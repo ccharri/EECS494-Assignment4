5d0bab54f06842df72c74d6eb61b08215ab720d6
Thomas Dorsey <twdorsey@umich.edu>
2013-12-06 14:56:14 -0500

    finally made camera rot work >:(

--- EECS494-Assignment4/Assets/CameraMovement.cs
+++ EECS494-Assignment4/Assets/CameraMovement.cs
@@ -7,6 +7,12 @@ public class CameraMovement : MonoBehaviour {
 	GameObject mainCamera;
 	float scrollArea = 25.0f;
 	float scrollSpeed = 25.0f;
+    float rotationSpeed = 0.50f;
+
+    float maxY = 5;
+    float minY = -5;
+    float maxX = 25;
+    float minX = -25;
 
 	// Use this for initialization
 	void Start () 
@@ -19,32 +25,66 @@ public class CameraMovement : MonoBehaviour {
 	void Update ()
 	{
 		rightDown = Input.GetMouseButton(1);
-
 		var mPosX = Input.mousePosition.x;
 		var mPosY = Input.mousePosition.y;
 
-
-		if (mPosX <= scrollArea) 
+        if(mPosX <= scrollArea || Input.GetKey(KeyCode.LeftArrow)) 
 		{
-			if(mainCamera.transform.position.x > (-45f/2f)) 
+            if(mainCamera.transform.position.x > minX) 
 				mainCamera.transform.position += (mainCamera.transform.right * -scrollSpeed * Time.deltaTime);
 		}
-		if (mPosX >= Screen.width-scrollArea)
+		if(mPosX >= Screen.width-scrollArea || Input.GetKey(KeyCode.RightArrow))
 		{
-			if(mainCamera.transform.position.x < (45f/2f))
+			if(mainCamera.transform.position.x < maxX)
 				mainCamera.transform.position += (mainCamera.transform.right * scrollSpeed * Time.deltaTime);
 		}
-		/*
-		if (mPosY <= scrollArea)
+		
+		if(mPosY <= scrollArea || Input.GetKey(KeyCode.UpArrow))
 		{
-			mainCamera.transform.position += (Vector3.Cross(mainCamera.transform.right, Vector3.up) * -scrollSpeed * Time.deltaTime);
+            if(mainCamera.transform.position.y < maxY)
+			    mainCamera.transform.position += (Vector3.Cross(mainCamera.transform.right, Vector3.up) * -scrollSpeed * Time.deltaTime);
 		}
-		if (mPosY >= Screen.height-scrollArea)
+		if(mPosY >= Screen.height-scrollArea || Input.GetKey(KeyCode.DownArrow))
 		{
-			mainCamera.transform.position += (Vector3.Cross(mainCamera.transform.right, Vector3.up) * scrollSpeed * Time.deltaTime);
+            if(mainCamera.transform.position.y < minY)
+			    mainCamera.transform.position += (Vector3.Cross(mainCamera.transform.right, Vector3.up) * scrollSpeed * Time.deltaTime);
 		}
-		*/
+
+        if(Input.GetKey(KeyCode.RightControl))
+        {
+            rotateCamera(mainCamera, -rotationSpeed * Time.deltaTime);
+        }
+        if(Input.GetKey(KeyCode.LeftControl))
+        {
+            rotateCamera(mainCamera, rotationSpeed * Time.deltaTime);
+            
+        }
 	}
+
+    void rotateCamera(GameObject camera, float radians)
+    {
+        Vector3 lookAt = getCameraLookAt(mainCamera, 0);
+        float xzRadius = new Vector3(lookAt.x - camera.transform.position.x, 0, lookAt.z - camera.transform.position.z).magnitude;
+        Debug.Log("radius: " + xzRadius);
+        float xzAngle = Mathf.Atan2(-lookAt.z + camera.transform.position.z, -lookAt.x + camera.transform.position.x);
+        Debug.Log("angle: " + xzRadius);
+        Vector3 newPos = new Vector3(lookAt.x + xzRadius * Mathf.Cos(xzAngle + radians), 
+                                     mainCamera.transform.position.y, 
+                                     lookAt.z + xzRadius * Mathf.Sin(xzAngle + radians));
+        mainCamera.transform.position = newPos;
+        Debug.Log("pos: " + newPos);
+        mainCamera.transform.LookAt(lookAt);
+    }
+
+    Vector3 getCameraLookAt(GameObject camera, float yHeight)
+    {
+        Vector3 cameraVector = camera.transform.forward.normalized;
+        float yDiff = camera.transform.position.y - yHeight;
+        float xzScaling = yDiff / -cameraVector.y;
+        return new Vector3(camera.transform.position.x + cameraVector.x * xzScaling, 
+                            yHeight, 
+                            camera.transform.position.z + cameraVector.z * xzScaling);
+    }
 
 	void LateUpdate()
 	{
