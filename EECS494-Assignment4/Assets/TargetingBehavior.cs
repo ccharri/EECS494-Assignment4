3fe81be70747b98b25c28fb769f4f585148106a9
Thomas Dorsey <twdorsey@umich.edu>
2013-12-08 16:01:34 -0500

    brick brick brick!

--- EECS494-Assignment4/Assets/TargetingBehavior.cs
+++ EECS494-Assignment4/Assets/TargetingBehavior.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public interface TargetingBehavior 
+interface TargetingBehavior 
 {
     //NOTE: All children of TargetingBehavior should define a "static T getInstance()"
 
--- EECS494-Assignment4/Assets/TargetingBehaviorProjectile.cs
+++ EECS494-Assignment4/Assets/TargetingBehaviorProjectile.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public interface TargetingBehaviorProjectile
+interface TargetingBehaviorProjectile
 {
     //NOTE: All children of TargetingBehavior should define a "static T getInstance()"
 
