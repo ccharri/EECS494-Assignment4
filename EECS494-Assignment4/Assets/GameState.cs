ed17649f44508f1d934ce00ed00ea0a172efcc45
Christopher Harris <ccharri@umich.edu>
2013-12-10 19:16:19 -0500

    Hard code player 2

--- EECS494-Assignment4/Assets/GameState.cs
+++ EECS494-Assignment4/Assets/GameState.cs
@@ -84,7 +84,7 @@ public class GameState : MonoBehaviour
         }
         else
         {
-            Camera.main.transform.position = new Vector3(-pos.x, pos.y, pos.z);
+            Camera.main.transform.position = new Vector3(55, pos.y, pos.z);
 			Camera.main.transform.Rotate(120, 180 ,0);
             incomeTimer.gameObject.transform.Rotate(Vector3.up, 180.0f);
             player1Terrain.tag = "Unbuildable";
